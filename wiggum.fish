6d3278c72e540d2ba9638b2f7dac866c61861ffe
Andrew Monks <a@monks.co>
2026-01-20 23:09:24 -0600

    wiggum fix

--- wiggum.fish
+++ wiggum.fish
@@ -3,29 +3,40 @@ function next
 end
 
 function fix --argument-names todo_id
+    echo ">> fixing $todo_id"
     jj new main
+    echo ">> made empty change"
     set -l change_id (jj show @ -T change_id)
-    echo "starting session for $todo_id"
-    cd (incr session start "$todo_id" --rev=$change_id)
+    echo ">> change_id=$change_id"
+    set -l workspace_dir (incr session start "$todo_id" --rev=$change_id) || return 1
+    echo ">> workspace_dir=$workspace_dir"
+    pushd $workspace_dir
     set -l prompt "complete this task:"\n\n"$(incr todo show $todo_id)"
+    echo ">> prompt:"
     echo "$prompt"
+    echo "<< prompt"
     echo "$prompt" | opencode run
+    echo ">> opencode done"
 
     if ! go test ./...
-      # incr session fail $todo_id
+      incr session fail $todo_id
       echo "tests failed; rejecting change"
+      popd
       return
     end
+    echo ">> tests passed"
 
-    echo "success; committing"
     set -l todo_json "$(incr todo show --json $todo_id)"
     set -l todo_title "$(echo "$todo_json" | jq -r first.title)"
     set -l todo_description "$(echo "$todo_json" | jq -r first.description)"
+    echo ">> todo_title=$todo_title"
+    echo ">> committing"
     jj commit --message="$todo_title"\n\n"$todo_description"
-    jj bookmark move main --to @-
+    echo ">> committed"
 
-    echo "done"
-    # incr session done $todo_id
+    incr session done $todo_id
+    echo ">> closed session"
+    popd
 end
 
 function wiggum
@@ -36,6 +47,7 @@ function wiggum
       break
     end
     fix $todo_id || break
+    jj bookmark move main --to @-
   end
 end
 
